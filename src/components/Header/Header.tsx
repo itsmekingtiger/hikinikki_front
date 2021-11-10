3300dec7a8e008c2750f3da79b349b8e4cd1a039
cmsong <cmsong@shinasys.com>
2021-11-10 18:23:03 +0900

    style: add header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ export interface HeaderProps {
 function Header({ title }: HeaderProps) {
     return (
         <header className="App-header">
-            <h1 className="text-xl">{title}</h1>
+            <h1 className="text-3xl text-center font-serif">{title}</h1>
         </header>
     );
 }
