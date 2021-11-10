245b6d497e8b7f73359b789893b4f976282e4731
cmsong <cmsong@shinasys.com>
2021-11-10 16:23:54 +0900

    refactor: change Header title to take  by props

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 // import './Header.css';
 
-function Header() {
+export interface HeaderProps {
+    title: string
+}
+
+function Header({ title }: HeaderProps) {
     return (
         <header className="App-header">
-            <h1 className="text-xl">Hiki Nikki</h1>
+            <h1 className="text-xl">{title}</h1>
         </header>
     );
 }
