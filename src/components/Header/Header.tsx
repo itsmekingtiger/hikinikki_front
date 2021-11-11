c0798a59824ce264680f099c060ecf77fa77da9c
cmsong <cmsong@shinasys.com>
2021-11-11 15:26:31 +0900

    style: add guide about shortcut

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,9 @@ export interface HeaderProps {
 
 function Header({ title }: HeaderProps) {
     return (
-        <header className="m-6">
-            <h1 className="text-3xl text-center font-serif">{title}</h1>
+        <header className="m-6 text-center">
+            <h1 className="text-3xl font-serif">{title}</h1>
+            <p className="italic text-gray-400">Press <kbd>Alt</kbd> + <kbd>N</kbd> to add new nikki.</p>
         </header>
     );
 }
