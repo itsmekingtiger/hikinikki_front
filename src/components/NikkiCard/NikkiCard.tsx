b223ee86141d81c244bffe715f5a8fcf58dc4030
cmsong <cmsong@shinasys.com>
2021-12-09 17:49:49 +0900

    feat: Moment를 사용하여 최근 1일간 상대적인 시간을 표시하도록 변경

--- src/components/NikkiCard/NikkiCard.tsx
+++ src/components/NikkiCard/NikkiCard.tsx
@@ -1,11 +1,15 @@
+import moment from 'moment';
+import 'moment/locale/ko';
 import React from 'react';
 import { Nikki } from '../../models/nikki';
+
 
 export interface NikkiCardProps {
     nikki: Nikki
 }
 
 function NikkiCard({ nikki }: NikkiCardProps) {
+
     return (
         // apply card design
         <div className="p-3 m-6 max-w-sm mx-auto bg-white rounded-xl border-2 border-black space-x-4">
@@ -16,11 +20,17 @@ function NikkiCard({ nikki }: NikkiCardProps) {
 
             {/* bottom line, include created date */}
             <div className="text-right text-sm text-gray-400">
-                {nikki.createdAt}
+                {transReadableDate(nikki.createdAt)}
             </div>
 
         </div>
     );
+}
+
+function transReadableDate(createdAt: string): string {
+    const recentOffset = moment().subtract({ hour: 12 });
+    const m = moment(createdAt);
+    return m.isBefore(recentOffset) ? m.format('llll') : m.fromNow();
 }
 
 export default NikkiCard
